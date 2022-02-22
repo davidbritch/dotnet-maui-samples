980fb3a128650b2905318f31dad307fcd3e4f73c
David Britch <davidbritch@users.noreply.github.com>
2022-02-22 10:01:30 +0000

    BindableLayouts.

---
+++ UserInterface/Layouts/BindableLayoutDemos/BindableLayoutDemos/MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace BindableLayoutDemos;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("ionicons.ttf", "Ionicons");
+				fonts.AddFont("fa-solid-900.ttf", "fa-solid-900");
+			});
+
+		return builder.Build();
+	}
+}
