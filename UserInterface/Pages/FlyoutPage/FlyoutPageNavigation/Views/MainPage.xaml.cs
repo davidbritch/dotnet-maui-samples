fe91e4aaf1beada4c01deb9c8a28181a735368a7
David Britch <davidbritch@users.noreply.github.com>
2022-03-15 10:45:21 +0000

    FlyoutPage

---
+++ UserInterface/Pages/FlyoutPage/FlyoutPageNavigation/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace FlyoutPageNavigation;
+
+public partial class MainPage : FlyoutPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        flyoutPage.listView.ItemSelected += OnItemSelected;
+    }
+
+    void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        var item = e.SelectedItem as FlyoutPageItem;
+        if (item != null)
+        {
+            Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+            flyoutPage.listView.SelectedItem = null;
+            IsPresented = false;
+        }
+    }
+}
