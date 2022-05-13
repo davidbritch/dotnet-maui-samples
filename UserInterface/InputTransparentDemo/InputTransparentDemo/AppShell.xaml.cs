30505798a8b29bb65b3c94c81e6c5a2cafd9b358
David Britch <davidbritch@users.noreply.github.com>
2022-05-13 13:08:01 +0100

    New sample.

---
+++ UserInterface/InputTransparentDemo/InputTransparentDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace InputTransparentDemo;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ UserInterface/InputTransparentDemo/InputTransparentDemo/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace InputTransparentDemo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void OnClickFail(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Failure; You shouldn't have been able to interact with that.");
+        DisplayAlert("Failure", "You shouldn't have been able to interact with that.", "OK");
+    }
+
+    void OnClickSuccess(object sender, EventArgs e)
+    {
+        Debug.WriteLine("Success; That should have worked, and it did!");
+        DisplayAlert("Success", "That should have worked, and it did!", "OK");
+    }
+}
