d1b9e63bf319bad674152e45445aa4b88c7cbe12
David Britch <davidbritch@users.noreply.github.com>
2022-06-13 14:41:19 +0100

    Bring inline with doc.

--- XAML/XamlSamples/XamlSamples/ViewModels/ClockViewModel.cs
+++ XAML/XamlSamples/XamlSamples/ViewModels/ClockViewModel.cs
@@ -1,35 +1,35 @@
 ﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace XamlSamples
 {
     public class ClockViewModel : INotifyPropertyChanged
     {
-        DateTime dateTime;
+        DateTime _dateTime;
+        Timer _timer;
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTime DateTime
+        {
+            get => _dateTime;
+            set
+            {
+                if (_dateTime != value)
+                {
+                    _dateTime = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         public ClockViewModel()
         {
             this.DateTime = DateTime.Now;
-            Timer timer = new Timer(new TimerCallback((s) => this.DateTime = DateTime.Now), null, 0, 1000);
+            _timer = new Timer(new TimerCallback((s) => this.DateTime = DateTime.Now), null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
         }
 
-        public DateTime DateTime
-        {
-            get
-            {
-                return dateTime;
-            }
-            set
-            {
-                if (dateTime != value)
-                {
-                    dateTime = value;
-                    if (PropertyChanged != null)
-                    {
-                        PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
-                    }
-                }
-            }
-        }
+        ~ClockViewModel() => _timer.Dispose();
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
