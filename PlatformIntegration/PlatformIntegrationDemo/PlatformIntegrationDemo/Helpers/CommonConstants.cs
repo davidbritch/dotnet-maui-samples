aea1dcee42efae2cd87f1bf970f5f9aaa91fe104
David Britch <davidbritch@users.noreply.github.com>
2023-10-27 12:50:17 +0100

    Essentials sample.

---
+++ PlatformIntegration/PlatformIntegrationDemo/PlatformIntegrationDemo/Helpers/CommonConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace PlatformIntegrationDemo.Helpers
+{
+    public static class CommonConstants
+    {
+        internal const string AppCenterAndroid = "AC_ANDROID";
+        internal const string AppCenteriOS = "AC_IOS";
+        internal const string AppCenterUWP = "AC_UWP";
+    }
+}
+
