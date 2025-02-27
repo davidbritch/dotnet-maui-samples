1d73a6a324d333f68be7ce93966f11f8bd450938
David Britch <davidbritch@users.noreply.github.com>
2025-02-27 10:29:13 +0000

    Update to .NET 9

---
+++ Fundamentals/ResourceDictionaryDemo/MyThemes/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace MyThemes;
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Location { get; set; }
+
+    public Person()
+    {
+    }
+
+    public Person(string name, int age, string location)
+    {
+        Name = name;
+        Age = age;
+        Location = location;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
